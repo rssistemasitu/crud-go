868db587aa4be0b882231908dec98faec4853c27
Rogerio Ferreira da Silva <rogeriofatecitu@gmail.com>
2023-10-25 15:55:30 -0300

    criando dtos de request e response e atualizando o controller createUser

--- internal/controller/createUser.go
+++ internal/controller/createUser.go
@@ -1,7 +1,21 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rssistemasitu/crud-go/internal/controller/model/request"
+	"github.com/rssistemasitu/crud-go/internal/rest_err"
+)
 
 func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
 
+	}
+	fmt.Print(userRequest)
 }
