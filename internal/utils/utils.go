d5a736b95e885608a808aec0ccc0c90ddb4a3ed0
Rogerio Ferreira da Silva <rogeriofatecitu@gmail.com>
2023-11-05 11:34:42 -0300

    atualizando arquivo de variavel de ambiente

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -4,19 +4,25 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 
 	"github.com/joho/godotenv"
 )
 
+const projectDirName = "crud-go"
+
 func GetEnvVariable(logName string) string {
-	err := godotenv.Load("../configs/.env")
-	if err != nil {
-		err = godotenv.Load("configs/.env")
-	}
+	// Garantir que busque o .env independente de onde chamar
+	projectName := regexp.MustCompile(`^(.*` + projectDirName + `)`)
+	currentWorkDirectory, _ := os.Getwd()
+	rootPath := projectName.Find([]byte(currentWorkDirectory))
+
+	err := godotenv.Load(string(rootPath) + `/.env`)
 
 	if err != nil {
 		result := fmt.Sprintf("Error loading .env file to get %s", logName)
 		log.Fatal(result)
 	}
+
 	return os.Getenv(logName)
 }
